cbf57c29f8ea074851c646cca4aaafae1b73744f
alexis <alex021994@gmail.com>
2014-08-27 08:28:36 +0000

    optimized system call and parsing

--- source/networking/BandwidthMonitor.py
+++ source/networking/BandwidthMonitor.py
@@ -1,32 +1,16 @@
 #This program checks for the total throughput of each wireless and ethernet network card over the current session and their current bandwidth (up and down) by printing them to the console
+#this program assumes 'netcat' is installed
+import subprocess
 CONST_IFCONFIG_DOWN_SUBSTR = "RX bytes:"
 CONST_IFCONFIG_UP_SUBSTR = "TX bytes:"
 
+
+
 #returns a tuple in the form (int downloaded, int uploaded) for a given network card (eth0,lo or wlan0)
-def parseIfconfig(outIfconfig,networkCard):
-	networkCardConfig = outIfconfig[outIfconfig.find(networkCard):]
+def parseRxTx(networkCard):
+	downstr = subprocess.check_output("cat /sys/class/net/"+networkCard+"/statistics/rx_bytes", shell=True)
+	upstr = subprocess.check_output("cat /sys/class/net/"+networkCard+"/statistics/tx_bytes", shell=True)
 
-	indexDownload = networkCardConfig.find(CONST_IFCONFIG_DOWN_SUBSTR) + len(CONST_IFCONFIG_DOWN_SUBSTR) #find the index to begin the parse
-	indexupload = networkCardConfig.find(CONST_IFCONFIG_UP_SUBSTR) + len(CONST_IFCONFIG_UP_SUBSTR)
-	
-
-	i=0
-	downstr=""
-	while networkCardConfig[indexDownload+i].isdigit(): #parsing the 'downloaded part'
-		downstr+=networkCardConfig[indexDownload+i]
-		i+=1
-	
-	i=0
-	upstr=""
-	while networkCardConfig[indexupload+i].isdigit(): #parsing the 'uploaded part'
-		upstr+=networkCardConfig[indexupload+i]
-		i+=1
-	
 	return int(downstr), int(upstr)
 
-
-
-import subprocess
-output = subprocess.check_output("ifconfig", shell=True)
-
-print parseIfconfig(output,"wlan0")
+print parseRxTx("wlan0")
